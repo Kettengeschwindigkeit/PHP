34a420be20affa98f2ff8345766c0fdc81c5155c
Kettengeschwindigkeit <666damned999@mail.ru>
2023-12-03 22:35:44 +0300

    if, elseif, else, switch-case

--- 01.php
+++ 01.php
@@ -1,6 +1,8 @@
 <?php
+// Установка PHP 8.x, php.ini и первый запуск программы. Окружение для разработки
+
 $foo = 55;
 $bar = 77;
 
-echo $foo + $bar;
-echo 'Hello';
+echo $foo + $bar;                   // -> 132
+echo 'Hello';                       // -> Hello
--- 02.php
+++ 02.php
@@ -1,43 +1,43 @@
 <?php
-// Комментарий
+// Основы синтаксиса, переменные, базовые операции PHP
 
-echo 4;
-echo (4);
+echo 4;                                               // -> 4
+echo (4);                                             // -> 4 
 echo "\n";
-echo '\n';
+echo '\n';                                            // -> \n
 echo PHP_EOL;
-echo '5';
+echo '5';                                             // -> 5
 echo "\n";
-echo 4, 5, 'Hello';
+echo 4, 5, 'Hello';                                   // -> 45Hello
 
-echo "\n\n========== Вывод переменных ==========\n\n";
+echo "\n\n==================== Вывод переменных ====================\n\n";
 
 $foo = 15;
 $bar = 7;
-echo $foo + $bar;
+echo $foo + $bar;                                     // -> 22
 echo "\n";
-echo $foo + 88;
+echo $foo + 88;                                       // -> 103
 echo "\n";
-echo $foo - $bar;
+echo $foo - $bar;                                     // -> 8
 echo "\n";
-echo $foo * $bar;
+echo $foo * $bar;                                     // -> 105
 echo "\n";
-echo $foo / $bar;
+echo $foo / $bar;                                     // -> 2.1428571428571
 echo "\n";
-echo $foo % $bar;
+echo $foo % $bar;                                     // -> 1
 echo "\n";
 
 $s1 = 'Hi';
 $s2 = 'Test';
-// echo $s1 + $s2;                                                                                                    PHP Fatal error: Uncaught TypeError: Unsupported operand types: string + string
-echo $s1 . $s2;                                                                                                                                                                       // конкатенация
+// echo $s1 + $s2;                                                                                                   PHP Fatal error: Uncaught TypeError: Unsupported operand types: string + string
+echo $s1 . $s2;                                       // -> HiTest                                                                                                                   // конкатенация
 echo "\n";
-echo $s1 . '5555';
-echo $s1 . 5555;
+echo $s1 . '5555';                                    // -> Hi5555
+echo $s1 . 5555;                                      // -> Hi5555
 echo "\n";
-echo '15 + 7 = ' . ($foo + $bar);
+echo '15 + 7 = ' . ($foo + $bar);                     // -> 15 + 7 = 22
 
-echo "\n\n========== Дроби ==========\n\n";
+echo "\n\n========================== Дроби =========================\n\n";
 
 $d = 8.5;
 $e = 13.2;
